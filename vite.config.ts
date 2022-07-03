d9163bba617a07e4c323bbb94d765cb2839a9789
bang <sqibang@gmail.com>
2022-07-03 14:04:19 +0800

    chore: config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import handlebars from 'vite-plugin-handlebars'
 import { resolve } from 'path'
 import minify from 'vite-plugin-minify'
+import legacy from '@vitejs/plugin-legacy'
 
 export default {
   plugins: [
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
     handlebars({
       compileOptions: {
         // Example config option: avoid auto-indenting partials
