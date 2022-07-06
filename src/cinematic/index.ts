3248e5a5f52d2e6f7916674ea28e33f64a9b4219
hooray <lyl@ik.me>
2022-07-06 18:29:59 +0800

    feat: control video

--- src/cinematic/index.ts
+++ src/cinematic/index.ts
@@ -1,41 +1,90 @@
 import './style.scss'
 import { gsap } from '../gsap'
 
-// 设置默认值
-export const createVindaTimeline = () => {
-  gsap.to('.blue h1', {
-    rotate: '180deg',
-    repeat: 4,
-
-    // duration: 5000,
-  })
-  const tl = gsap.timeline({
+export const createCinematicTimeline = () => {
+  const cinematic = document.getElementById('cinematic')
+  const mode = cinematic!.querySelector('.cinematic-mode')
+  const headline = cinematic!.querySelector('.headline')
+  const h3 = headline!.querySelector('h3')
+  const subheadline = headline!.querySelector('.subheadline')
+  const frameView = cinematic!.querySelector('.frame-view')
+  const frameBottom = cinematic!.querySelector('.frame-bottom')
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: mode,
+        start: 'top 90%',
+        end: 'bottom 60%',
+        scrub: true,
+      },
+    })
+    .from(h3, {
+      opacity: 0,
+      y: 20,
+    })
+    .from(
+      subheadline,
+      {
+        opacity: 0,
+        y: 20,
+      },
+      '>',
+    )
+    .from(
+      frameView,
+      {
+        opacity: 0,
+        y: 20,
+      },
+      '>',
+    )
+    .from(
+      frameBottom,
+      {
+        opacity: 0,
+        y: 30,
+      },
+      '>',
+    )
+  let videoPlaying = false
+  const frameSticky = cinematic!.querySelector('.frame-sticky')
+  const cinematicVideo = document.getElementById(
+    'cinematicVideo',
+  ) as HTMLVideoElement | null
+  const cinematicControl = document.getElementById(
+    'cinematicControl',
+  ) as HTMLButtonElement | null
+  if (cinematicControl && cinematicVideo) {
+    cinematicControl.addEventListener('click', () => {
+      if (videoPlaying) {
+        cinematicVideo.pause()
+        cinematicControl.textContent = 'Pause'
+      } else {
+        cinematicVideo.play()
+        cinematicControl.textContent = 'Play'
+      }
+      videoPlaying = !videoPlaying
+    })
+  }
+  gsap.timeline({
     scrollTrigger: {
-      trigger: '.orange',
-      start: 'center center',
+      trigger: frameSticky,
+      start: 'top 10%',
       scrub: true,
       pin: true,
+      markers: true,
+      onToggle: (self) => {
+        if (cinematicVideo !== null && cinematicControl !== null) {
+          if (self.isActive) {
+            cinematicControl.textContent = 'Play'
+            cinematicVideo.play()
+            videoPlaying = true
+          } else {
+            videoPlaying = false
+            cinematicVideo.pause()
+          }
+        }
+      },
     },
   })
-  tl.fromTo(
-    '#orange-content',
-    {
-      y: -500,
-      scale: 0.5,
-      opacity: 0,
-    },
-    {
-      y: 0,
-      opacity: 1,
-      scale: 1,
-    },
-  )
-  tl.to(
-    '#orange-content code',
-    {
-      fontSize: '30px',
-      color: '#fff',
-    },
-    1,
-  )
 }
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,12 @@ import './style.scss'
 import 'lazysizes'
 import { createHeroTimeline } from './hero'
 import { createCatTimeline } from './cat'
-import { createVindaTimeline } from './cinematic'
+import { createCinematicTimeline } from './cinematic'
 import './colors'
 
 // https://zh.javascript.info/onload-ondomcontentloaded
 window.addEventListener('load', function () {
   // run timeline
   createHeroTimeline()
-
-  const ctl = createCatTimeline()
-
-  createVindaTimeline()
+  createCinematicTimeline()
 })
