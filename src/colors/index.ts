ae946438d971f93154bb5bc2f6c2b7b256e3889f
PCQQQ <pcqdzh5151@gmail.com>
2022-07-07 15:04:47 +0800

    feat: colors section timeline

--- src/colors/index.ts
+++ src/colors/index.ts
@@ -16,7 +16,11 @@ colorsBtns.forEach((btn, index) => {
           gsap
             .timeline()
             .to(item, { zIndex: 2 })
-            .from(item, { opacity: 0, duration: 0.7, ease: 'power1.out' }, '<')
+            .from(
+              item,
+              { opacity: 0, duration: 0.6, ease: 'power1.inOut' },
+              '<',
+            )
         } else {
           if (item.classList.contains('active')) {
             gsap.to(item, { zIndex: 1 })
@@ -28,4 +32,39 @@ colorsBtns.forEach((btn, index) => {
       })
     }
   })
+})
+
+colorsItems.forEach((item) => {
+  const box = item.querySelectorAll('.box')
+  // gsap
+  //   .timeline({
+  //     scrollTrigger: {
+  //       trigger: box,
+  //       start: 'top 90%',
+  //       end: 'bottom 10%',
+  //       markers: true,
+  //       scrub: true,
+  //     },
+  //   })
+  //   .from(box, { y: 100, opacity: 0, stagger: 0.5, duration: 1 })
+  const info1 = item.querySelector('.info1')
+  const info2 = item.querySelector('.info2')
+  const info3 = item.querySelector('.info3')
+  const info4 = item.querySelector('.info4')
+  const info5 = item.querySelector('.info5')
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: box,
+        start: 'top 90%',
+        end: 'bottom 10%',
+        markers: true,
+        scrub: true,
+      },
+    })
+    .from(info1, { y: 100, opacity: 0, duration: 1.5 })
+    .from(info2, { y: 100, opacity: 0, duration: 1.5 })
+    .from(info3, { y: 150, opacity: 0, duration: 2 }, '+=0.5')
+    .from(info4, { y: 100, opacity: 0, duration: 1.5 }, '-=0.5')
+    .from(info5, { y: 100, opacity: 0, duration: 1.5 })
 })
