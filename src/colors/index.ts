ccb554b3e90285822f65d25c933d85d03870f285
PCQQQ <pcqdzh5151@gmail.com>
2022-07-07 18:42:33 +0800

    refactor: 颜色区域动画优化

--- src/colors/index.ts
+++ src/colors/index.ts
@@ -2,11 +2,11 @@ import './style.scss'
 import { gsap } from '../gsap'
 
 const colorsBtns = document.querySelectorAll('#colors .color-btn')
+const colorsContainer = document.querySelectorAll('#colors .item-container')
 const colorsItems = document.querySelectorAll('#colors .item-container .item')
 colorsBtns.forEach((btn, index) => {
   btn.addEventListener('click', () => {
     if (!btn.classList.contains('active')) {
-      console.log({ index })
       colorsBtns.forEach((b) => b.classList.remove('active'))
       btn.classList.toggle('active')
 
@@ -55,16 +55,16 @@ colorsItems.forEach((item) => {
   gsap
     .timeline({
       scrollTrigger: {
-        trigger: box,
-        start: 'top 90%',
-        end: 'bottom 10%',
+        trigger: colorsContainer,
+        start: 'top 80%',
+        end: 'bottom 60%',
         markers: true,
         scrub: true,
       },
     })
-    .from(info1, { y: 100, opacity: 0, duration: 1.5 })
-    .from(info2, { y: 100, opacity: 0, duration: 1.5 })
-    .from(info3, { y: 150, opacity: 0, duration: 2 }, '+=0.5')
-    .from(info4, { y: 100, opacity: 0, duration: 1.5 }, '-=0.5')
-    .from(info5, { y: 100, opacity: 0, duration: 1.5, delay: 2 }, '>')
+    .from(info1, { y: 100, opacity: 0 })
+    .from(info2, { y: 100, opacity: 0 }, '-=0.3')
+    .from(info3, { y: 100, opacity: 0 }, '-=0.2')
+    .from(info4, { y: 100, opacity: 0 }, '-=0.4')
+    .from(info5, { y: 100, opacity: 0 })
 })
