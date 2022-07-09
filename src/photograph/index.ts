18b0d394248913c2793fc98e886ceeb9d4f75434
PCQQQ <pcqdzh5151@gmail.com>
2022-07-09 16:59:07 +0800

    feat: 拍摄照片区域最后动画

--- src/photograph/index.ts
+++ src/photograph/index.ts
@@ -39,4 +39,36 @@ export const createPhotographTimeline = () => {
       ease: 'power3',
     })
   })
+
+  //   const pic9Clip = section!.querySelector('.pic9-clip')
+  const pic9 = section!.querySelector('.pic9')
+  const phoneBox = section!.querySelector('.pic-phone')
+  const phoneBar = section!.querySelector('.pic-bar')
+
+  gsap.to(pic9, {
+    duration: 0,
+    scale: 1.4,
+  })
+  gsap.to(phoneBox, {
+    duration: 0,
+    scale: 3.4,
+  })
+
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: '.memories-mode',
+        start: '-150',
+        // end: 'bottom 90%',
+        markers: true,
+        scrub: 1,
+        pin: true,
+        pinSpacing: true,
+      },
+    })
+    .to(phoneBox, { scale: 1, yPercent: -50, duration: 1.6 })
+    .to(pic9, { borderRadius: 100, duration: 0 }, '<')
+    .to(pic9, { scale: 0.45, duration: 1.6 }, '<+0.05')
+    .from(phoneBar, { opacity: 0, duration: 1 }, '-=0.2')
+    .from('.memories-text', { opacity: 0, y: 50, duration: 0.2 })
 }
