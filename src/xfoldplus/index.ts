4ec61b77c26cf4c1d3a3dfa15731e7b41142b4fd
hooray <lyl@ik.me>
2023-01-04 10:38:10 +0800

    chore

---
+++ src/xfoldplus/index.ts
@@ -0,0 +1,15 @@
+import './style.scss'
+import 'lazysizes'
+import 'lazysizes/plugins/unveilhooks/ls.unveilhooks'
+import { createHeroTimeline } from '../hero'
+import { createCinematicTimeline } from '../cinematic'
+import '../colors'
+import { createPhotographTimeline } from '../photograph'
+
+// https://zh.javascript.info/onload-ondomcontentloaded
+window.addEventListener('load', function () {
+  // run timeline
+  createHeroTimeline()
+  createCinematicTimeline()
+  createPhotographTimeline()
+})
